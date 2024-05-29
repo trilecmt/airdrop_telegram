9638a9260b3d9795192c690fa4dcf25bb34e97e9
Tri Le <trilm3@supra.masangroup.com>
2024-05-29 13:40:26 +0700

    cexp v2 proxy

--- cexpv2.py
+++ cexpv2.py
@@ -5,14 +5,17 @@ import urllib.parse
 import json
 import time
 import re
+from datetime import datetime,timedelta
+import traceback
 
 user_info_url = "https://cexp.cex.io/api/getUserInfo"
 claim_taps_url = "https://cexp.cex.io/api/claimTaps"
 claim_farm_url = "https://cexp.cex.io/api/claimFarm"
 start_farm_url = "https://cexp.cex.io/api/startFarm"
+format_string = "%Y-%m-%dT%H:%M:%S.%fZ"
 
-def exec(url):
-    url='https://cexp.cex.io/#tgWebAppData=query_id%3DAAELjJx_AAAAAAuMnH9Whv9r%26user%3D%257B%2522id%2522%253A2140965899%252C%2522first_name%2522%253A%2522Tri%25202%2522%252C%2522last_name%2522%253A%2522Le%2522%252C%2522username%2522%253A%2522trilm208%2522%252C%2522language_code%2522%253A%2522en%2522%252C%2522allows_write_to_pm%2522%253Atrue%257D%26auth_date%3D1716571408%26hash%3Df028ad0ce7070e95c60d119f03b9c1eca8837a3b6b1bab227ea4a902a45f2da2&tgWebAppVersion=7.2&tgWebAppPlatform=ios&tgWebAppThemeParams=%7B%22bg_color%22%3A%22%23ffffff%22%2C%22text_color%22%3A%22%23000000%22%2C%22hint_color%22%3A%22%23707579%22%2C%22link_color%22%3A%22%233390ec%22%2C%22button_color%22%3A%22%233390ec%22%2C%22button_text_color%22%3A%22%23ffffff%22%2C%22secondary_bg_color%22%3A%22%23f4f4f5%22%2C%22header_bg_color%22%3A%22%23ffffff%22%2C%22accent_text_color%22%3A%22%233390ec%22%2C%22section_bg_color%22%3A%22%23ffffff%22%2C%22section_header_text_color%22%3A%22%23707579%22%2C%22subtitle_text_color%22%3A%22%23707579%22%2C%22destructive_text_color%22%3A%22%23e53935%22%7D'
+  
+def exec(url,proxy_url):
     if url is None:
         return
     print(50*"*")
@@ -31,8 +34,6 @@ def exec(url):
     headers = {
         "content-type": "application/json"
     }
-
-    # Define payload for the first POST request
     payload = {
         "devAuthData": uid,
         "authData": data,
@@ -41,111 +42,121 @@ def exec(url):
     }
 
     try:
-        # Make the first POST request to get user info
-        response = requests.post(user_info_url, headers=headers, data=json.dumps(payload))
-        user_info = response.json()
+        with requests.Session() as session:
+            if proxy_url is not None and proxy_url!='':  
+                session.proxies= {
+                    'http':proxy_url,
+                    'https':proxy_url
+                }
+                print('changed proxy...')
+            response = session.post("https://api.myip.com/",timeout=10, headers=headers,data=json.dumps(payload))
+            print(f"my ip:{response.json()['ip']}")
+            response = session.post(user_info_url,timeout=10, headers=headers,data=json.dumps(payload))
+            user_info = response.json()
+            print(f"app ip:{response.json()['data']['ip']}")
+            
+ 
+            if user_info["status"] != "ok":
+                print("Error: Couldn't fetch user data")
+                raise ValueError("Error: Couldn't fetch user data")
 
-        if user_info["status"] != "ok":
-            print("Error: Couldn't fetch user data")
-            raise ValueError("Error: Couldn't fetch user data")
+            if "userTelegramId" in user_info["data"]:
+                print("Account:", user_info["data"]["userTelegramId"])
 
-        if "userTelegramId" in user_info["data"]:
-            print("Account:", user_info["data"]["userTelegramId"])
+            if "username" in user_info["data"]:
+                print("Username:", user_info["data"]["username"])
 
-        if "username" in user_info["data"]:
-            print("Username:", user_info["data"]["username"])
+            if "balance" in user_info["data"]:
+                print("Balance:", user_info["data"]["balance"])
 
-        if "balance" in user_info["data"]:
-            print("Balance:", user_info["data"]["balance"])
+            if "availableTaps" in user_info["data"] and int(user_info["data"]["availableTaps"]) > 0:
+                print("Available Taps:", user_info["data"]["availableTaps"])
+                print("-> CLAIMING..")
 
-        if "availableTaps" in user_info["data"] and int(user_info["data"]["availableTaps"]) > 0:
-            print("Available Taps:", user_info["data"]["availableTaps"])
-            print("-> CLAIMING..")
+                claim_taps_url = "https://cexp.cex.io/api/claimTaps"
+                claim_taps_payload = {
+                    "devAuthData": uid,
+                    "authData": data,
+                    "data": {"taps": user_info["data"]["availableTaps"]}
+                }
+                response = requests.post(claim_taps_url, headers=headers, data=json.dumps(claim_taps_payload))
+                claim_taps_result = response.json()
 
-            claim_taps_url = "https://cexp.cex.io/api/claimTaps"
-            claim_taps_payload = {
-                "devAuthData": uid,
-                "authData": data,
-                "data": {"taps": user_info["data"]["availableTaps"]}
-            }
-            response = requests.post(claim_taps_url, headers=headers, data=json.dumps(claim_taps_payload))
-            claim_taps_result = response.json()
+                if claim_taps_result["status"] != "ok":
+                    print("Error: Couldn't claim taps")
+                    raise ValueError("Error: Couldn't claim taps")
 
-            if claim_taps_result["status"] != "ok":
-                print("Error: Couldn't claim taps")
-                raise ValueError("Error: Couldn't claim taps")
+                print("-> CLAIM SUCCESS!")
+                print("BALANCE:", claim_taps_result["data"]["balance"])
+                user_info["data"]["currentTapWindowFinishIn"] = claim_taps_result["data"]["currentTapWindowFinishIn"]
 
-            print("-> CLAIM SUCCESS!")
-            print("BALANCE:", claim_taps_result["data"]["balance"])
-            user_info["data"]["currentTapWindowFinishIn"] = claim_taps_result["data"]["currentTapWindowFinishIn"]
+            else:
+                print("No taps to claim!")
 
-        else:
-            print("No taps to claim!")
+            print("NEXT CLAIM:", str(int(user_info["data"]["currentTapWindowFinishIn"]) // 1000 // 3600) + "H" + str(int(user_info["data"]["currentTapWindowFinishIn"]) // 1000 % 60) + "M")
+            print("FARM REWARD:", user_info["data"]["farmReward"])
+            print("FARMING")
+            
+            if "farmStartedAt" in user_info["data"] and datetime.utcnow()> (datetime.strptime(user_info["data"]["farmStartedAt"], format_string)+timedelta(hours=4)):
+                print("-> CLAIMING")
+                claim_farm_url = "https://cexp.cex.io/api/claimFarm"
+                claim_farm_payload = {
+                    "devAuthData": uid,
+                    "authData": data,
+                    "data": {}
+                }
+                response = requests.post(claim_farm_url, headers=headers, data=json.dumps(claim_farm_payload))
+                claim_farm_result = response.json()
 
-        print("NEXT CLAIM:", str(int(user_info["data"]["currentTapWindowFinishIn"]) // 1000 // 3600) + "H" + str(int(user_info["data"]["currentTapWindowFinishIn"]) // 1000 % 60) + "M")
-        print("FARM REWARD:", user_info["data"]["farmReward"])
-        print("FARMING")
+                if "status" in claim_farm_result and claim_farm_result["status"] != "ok":
+                    print("Error: Couldn't claim farm")
+                    raise ValueError("Error: Couldn't claim farm")
 
-        if "farmStartedAt" in user_info["data"] and (int((time.time() * 1000 - int(user_info["data"]["farmStartedAt"])) / 1000) - 14400) > 0:
-            print("-> CLAIMING")
+                claim_farm_result["data"]["farmReward"] = "0.00"
+                user_info["data"]["farmReward"]= "0.00"
+                print("-> CLAIM:", claim_farm_result["data"]["claimedBalance"])
+                print("-> BALANCE:", claim_farm_result["data"]["balance"])
 
-            claim_farm_url = "https://cexp.cex.io/api/claimFarm"
-            claim_farm_payload = {
-                "devAuthData": uid,
-                "authData": data,
-                "data": {}
-            }
-            response = requests.post(claim_farm_url, headers=headers, data=json.dumps(claim_farm_payload))
-            claim_farm_result = response.json()
+            elif "farmStartedAt" in user_info["data"]:
+                print(f'-> Claim After:  {int(((datetime.strptime(user_info["data"]["farmStartedAt"], format_string)+timedelta(hours=4))-datetime.utcnow()).total_seconds()/60)} minutes')
 
-            if "status" in claim_farm_result and claim_farm_result["status"] != "ok":
-                print("Error: Couldn't claim farm")
-                raise ValueError("Error: Couldn't claim farm")
+            if "farmReward" in user_info["data"] and user_info["data"]["farmReward"] == "0.00":
+                print("-> FARM: STARTING")
 
-            claim_farm_result["data"]["farmReward"] = "0.00"
-            print("-> CLAIM:", claim_farm_result["data"]["claimedBalance"])
-            print("-> BALANCE:", claim_farm_result["data"]["balance"])
+                start_farm_url = "https://cexp.cex.io/api/startFarm"
+                start_farm_payload = {
+                    "devAuthData": uid,
+                    "authData": data,
+                    "data": {}
+                }
+                response = requests.post(start_farm_url, headers=headers, data=json.dumps(start_farm_payload))
+                start_farm_result = response.json()
 
-        elif "farmStartedAt" in user_info["data"]:
-            print("-> Claim After:", str(int((14400 - (int(time.time() * 1000) - int(user_info["data"]["farmStartedAt"])) / 1000) / 60)) + " minutes")
+                if "status" in start_farm_result and start_farm_result["status"] != "ok":
+                    print("Error: Couldn't start farm")
+                    raise ValueError("Error: Couldn't start farm")
 
-        if "farmReward" in user_info["data"] and user_info["data"]["farmReward"] == "0.00":
-            print("-> FARM: STARTING")
-
-            start_farm_url = "https://cexp.cex.io/api/startFarm"
-            start_farm_payload = {
-                "devAuthData": uid,
-                "authData": data,
-                "data": {}
-            }
-            response = requests.post(start_farm_url, headers=headers, data=json.dumps(start_farm_payload))
-            start_farm_result = response.json()
-
-            if "status" in start_farm_result and start_farm_result["status"] != "ok":
-                print("Error: Couldn't start farm")
-                raise ValueError("Error: Couldn't start farm")
-
-            print("-> FARMING")
-            print("->", start_farm_result["data"]["farmReward"])
+                print("-> FARMING")
+                print("->", start_farm_result["data"]["farmReward"])
 
     except Exception as e:
-        message = f"Error: {str(e)}"
-        status = "ERROR!"
+        
+        print(traceback.format_exc())
+        print("Status:ERROR!")
 
-    print(message)
-    print("Status:", status)
+    
     print(50*"*")
     print(50*"")
 
 if __name__=='__main__':
-    delay_time=input("Enter delay time:...")
+    delay_time=60 # 60s claim 1 nick
     while True:
         try:
-            df=pd.read_excel("account-cexp.xlsx",dtype={"url":str},sheet_name='cexp')
+            df=pd.read_excel("account.xlsx",dtype={"url":str},sheet_name='cexp')
             df=df[(~df['url'].isna()) & (df['url']!='')]
             df.reset_index(inplace=True)
             for idx,row in df.iterrows():
-                exec(row['url'])
+                exec(row['url'],row['proxy'])
                 time.sleep(delay_time)
                              
         except Exception as e:
