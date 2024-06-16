3e433b2c511b712c525d4c0237e94bec98ce8823
Tri Le <trile.cmt@gmail.com>
2024-06-16 22:43:41 +0700

    update

--- pixelvers.py
+++ pixelvers.py
@@ -1,4 +1,4 @@
-import asyncio
+import io
 import os
 import sys
 import json
@@ -41,6 +41,7 @@ class Data:
 
 class PixelTod:
     def __init__(self,proxies):
+        self.session=requests.Session()
         self.proxies=proxies
         self.DEFAULT_COUNTDOWN = 10 * 60  # 5 minutes
         self.INTERVAL_DELAY = 3  # seconds
@@ -62,77 +63,76 @@ class PixelTod:
     def data_parsing(self, data):
         return {key: value for key, value in (i.split('=') for i in unquote(data).split('&'))}
 
-    async def process_account(self, data, id_pets:str):
-        await self.get_me(data)
-        await self.daily_reward(data)
-        await self.get_mining_proccess(data)
-        await self.auto_buy_pet(data)
-        await self.auto_upgrade_pet(data)
+    def process_account(self, data, id_pets:str):
+        self.get_me(data)
+        self.daily_reward(data)
+        self.get_mining_proccess(data)
+        self.auto_buy_pet(data)
+        self.auto_upgrade_pet(data)
         if id_pets!="":
-            await self.daily_combo(data, id_pets)
+             self.daily_combo(data, id_pets)
             
-    async def api_call(self, url, data=None, headers=None, method='GET'):
-        async with aiohttp.ClientSession() as session:
-            for i in range(5):
-                try:
-                    if method == 'GET':
-                        if self.proxies is not None:
-                            res =await session.get(url, headers=headers,proxy=self.proxies)
-                        else:
-                            res =await session.get(url, headers=headers)
-                    elif method == 'POST':
-                        if self.proxies is not None:
-                            res =await session.post(url, headers=headers, data=data,proxy=self.proxies)
-                        else:
-                            res =await session.post(url, headers=headers, data=data)
+    def api_call(self, url, data=None, headers=None, method='GET'): 
+        for i in range(5):
+            try:
+                if method == 'GET':
+                    if self.proxies is not None:
+                        res = self.session.get(url, headers=headers,proxies=self.proxies)
                     else:
-                        raise ValueError(f'Unsupported method: {method}')
-                    
-                    if res.status == 401:
-                        self.log(f'{merah}{res.text}')
+                        res =self.session.get(url, headers=headers)
+                elif method == 'POST':
+                    if self.proxies is not None:
+                        res =self.session.post(url, headers=headers, data=data,proxies=self.proxies)
+                    else:
+                        res =self.session.post(url, headers=headers, data=data)
+                else:
+                    raise ValueError(f'Unsupported method: {method}')
+                
+                if res.status_code == 401:
+                    self.log(f'{merah}{res.text}')
 
-                    open('.http.log', 'a', encoding='utf-8').write(f'{res.text}\n')
-                    return res
-                except (requests.exceptions.ConnectionError, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout, requests.exceptions.Timeout):
-                    self.log(f'{merah}Connection error / connection timeout !')
-                    continue
+                open('.http.log', 'a', encoding='utf-8').write(f'{res.text}\n')
+                return res
+            except (requests.exceptions.ConnectionError, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout, requests.exceptions.Timeout):
+                self.log(f'{merah}Connection error / connection timeout !')
+                continue
 
-    async def get_me(self, data: Data):
+    def get_me(self, data: Data):
         url = 'https://api-clicker.pixelverse.xyz/api/users'
         headers = self.prepare_headers(data)
-        res = await self.api_call(url, None, headers)
-        if res.status not in [200,201]:
+        res =  self.api_call(url, None, headers)
+        if res.status_code not in [200,201]:
             return
-        data=await res.json()
+        data= res.json()
         balance = data.get('clicksCount', 'N/A')
         self.log(f'{hijau}Total Balance : {putih}{balance}')
 
-    async def daily_reward(self, data: Data):
+    def daily_reward(self, data: Data):
         url = 'https://api-clicker.pixelverse.xyz/api/daily-rewards'
         headers = self.prepare_headers(data)
-        res =await self.api_call(url, None, headers)
-        if res.status not in [200,201]:
+        res = self.api_call(url, None, headers)
+        if res.status_code not in [200,201]:
             return
-        data=await res.json()
+        data= res.json()
         if data.get('todaysRewardAvailable'):
             url_claim = 'https://api-clicker.pixelverse.xyz/api/daily-rewards/claim'
-            res =await self.api_call(url_claim, '', headers, method='POST')
-            if res.status not in [200,201]:
+            res = self.api_call(url_claim, '', headers, method='POST')
+            if res.status_code not in [200,201]:
                 return
-            data=await res.json()
+            data= res.json()
             amount = data.get('amount', 'N/A')
             self.log(f'{hijau}Success claim today reward : {putih}{amount}')
         else:
             self.log(f'{kuning}Already claim today reward !')
 
-    async def get_mining_proccess(self, data: Data):
+    def get_mining_proccess(self, data: Data):
         url = "https://api-clicker.pixelverse.xyz/api/mining/progress"
         headers = self.prepare_headers(data)
-        res =await self.api_call(url, None, headers)
+        res = self.api_call(url, None, headers)
         
-        if res.status  in [200,201]:
+        if res.status_code  in [200,201]:
             try:
-                response_json =await res.json()
+                response_json = res.json()
             except json.JSONDecodeError:
                 self.log(f'{merah}Failed to decode JSON response.')
                 return
@@ -143,10 +143,10 @@ class PixelTod:
             
             if available > min_claim:
                 url_claim = 'https://api-clicker.pixelverse.xyz/api/mining/claim'
-                res =await self.api_call(url_claim, '', headers, method='POST')
-                if res.status in [200,201]:
+                res = self.api_call(url_claim, '', headers, method='POST')
+                if res.status_code in [200,201]:
                     try:
-                        claim_response =await res.json()
+                        claim_response = res.json()
                     except json.JSONDecodeError:
                         self.log(f'{merah}Failed to decode JSON response.')
                         return
@@ -160,13 +160,13 @@ class PixelTod:
         else:
             self.log(f'{merah}Empty response from mining progress API.')
 
-    async def auto_buy_pet(self, data: Data):
+    def auto_buy_pet(self, data: Data):
         url = 'https://api-clicker.pixelverse.xyz/api/pets/buy?tg-id={self.tg_id}&secret={self.secret}'
         headers = self.prepare_headers(data)
-        res_buy_pet =await self.api_call(url, data=json.dumps({}), headers=headers, method='POST')
-        if res_buy_pet.status == 200 or res_buy_pet.status == 201:
+        res_buy_pet = self.api_call(url, data=json.dumps({}), headers=headers, method='POST')
+        if res_buy_pet.status_code == 200 or res_buy_pet.status_code == 201:
             try:
-                buy_pet_data =await res_buy_pet.json()
+                buy_pet_data = res_buy_pet.json()
                 pet_name = buy_pet_data.get('pet', {}).get('name', 'Unknown')
                 self.log(f'{hijau}Successfully buy a new pet! You got {kuning}{pet_name}!')
             except json.JSONDecodeError:
@@ -174,13 +174,13 @@ class PixelTod:
         else:
             self.log(f'{merah}Not yet time to buy another pet or Insufficient points')
 
-    async def auto_upgrade_pet(self, data: Data):
+    def auto_upgrade_pet(self, data: Data):
         url = 'https://api-clicker.pixelverse.xyz/api/pets'
         headers = self.prepare_headers(data)
-        res =await self.api_call(url, None, headers)
-        if res.status not in [200,201]:
+        res = self.api_call(url, None, headers)
+        if res.status_code not in [200,201]:
             return
-        data= await res.json()
+        data=  res.json()
         pets = data.get('data', [])
         if pets:
             pets_info = ', '.join([f'{pet["name"].strip()} Lv.{pet["userPet"]["level"]}' for pet in pets])
@@ -191,24 +191,24 @@ class PixelTod:
                 pet_id = pet['userPet']['id']
                 self.log(f'{putih}Selecting Pet ID: {hijau}{pet_id} ({pet_name} Lv.{pet_level})')
                 url_upgrade = f'https://api-clicker.pixelverse.xyz/api/pets/user-pets/{pet_id}/level-up'
-                res_upgrade =await self.api_call(url_upgrade, '', headers, method='POST')
-                if res_upgrade.status == 200 or res_upgrade.status == 201:
+                res_upgrade = self.api_call(url_upgrade, '', headers, method='POST')
+                if res_upgrade.status_code == 200 or res_upgrade.status_code == 201:
                     self.log(f'{hijau}Success Upgrading Pet! Pet ID: {putih}{pet_id} ({pet_name})')
                 else:
-                    data=await res_upgrade.json()
+                    data= res_upgrade.json()
                     error_message = data.get('message', 'Unknown error')
                     self.log(f'{merah}Failed to Upgrade Pet! Pet ID: {putih}{pet_id}, {merah}Error: {error_message}')
         else:
             self.log(f'{kuning}No Pets Available for Upgrade')
 
-    async def daily_combo(self, data: Data, id_pets):
+    def daily_combo(self, data: Data, id_pets):
         url_current_game = "https://api-clicker.pixelverse.xyz/api/cypher-games/current"
         headers = self.prepare_headers(data)
-        res_current_game =await self.api_call(url_current_game, None, headers)
+        res_current_game = self.api_call(url_current_game, None, headers)
         
-        if res_current_game.status  in [200,201]:
+        if res_current_game.status_code  in [200,201]:
             try:
-                game_data =await res_current_game.json()
+                game_data = res_current_game.json()
             except json.JSONDecodeError:
                 self.log(f'{merah}Failed to decode JSON response from current game API.')
                 return
@@ -223,11 +223,11 @@ class PixelTod:
 
                 url_answer = f"https://api-clicker.pixelverse.xyz/api/cypher-games/{game_id}/answer"
                 headers['Content-Type'] = 'application/json'
-                res_answer =await self.api_call(url_answer, data=json.dumps(payload), headers=headers, method='POST')
+                res_answer = self.api_call(url_answer, data=json.dumps(payload), headers=headers, method='POST')
 
-                if res_answer.status == 200 or res_answer.status == 201:
+                if res_answer.status_code == 200 or res_answer.status_code == 201:
                     try:
-                        answer_data =await res_answer.json()
+                        answer_data = res_answer.json()
                         reward_amount = answer_data.get('rewardAmount', 'N/A')
                         self.log(f'{hijau}Successfully submitted the daily combo! Reward Amount: {reward_amount}')
                     except json.JSONDecodeError:
@@ -255,13 +255,14 @@ class PixelTod:
         print(f"{hitam}[{now}]{reset} {self.profile_id}{message}")
 
 
-async def exec(profile):
+def exec(profile):
     try:
-        app = PixelTod(utils.get_proxies(profile['proxy'],type=0))
-        response =await app.api_call(url="https://httpbin.org/ip",headers={"content-type": "application/json"})
-        if response.status!=200:
+        app = PixelTod(utils.get_proxies(profile['proxy'],type=1))
+        print_message(f"✅ #{profile['name']} Bắt đầu")
+        response = app.api_call(url="https://httpbin.org/ip",headers={"content-type": "application/json"})
+        if response.status_code!=200:
             return print_message(f"❌ #{profile['name']} Proxy không lấy được IP")
-        data=await response.json()
+        data= response.json()
         app.profile_id=f"{profile["name"]}[{data['origin']}]"   
         data_parse = app.data_parsing(profile['query'])
         user = json.loads(data_parse['user'])
@@ -269,7 +270,7 @@ async def exec(profile):
         username = user.get('username')
         secret = app.get_secret(userid)
         new_data = Data(profile['query'], userid, username, secret)
-        await app.process_account(new_data, profile['daily_combo'])
+        app.process_account(new_data, profile['daily_combo'])
 
     except Exception as e:
           print_message(f"❌ Lỗi khi xử lí profile {profile['name']}")
@@ -277,11 +278,11 @@ async def exec(profile):
     finally:
         print_message(f"✅ #{profile['name']} Kết thúc.Move next")
 
-async def limited_exec(semaphore, profile):
-    async with semaphore:
-        await exec(profile)
+def limited_exec(semaphore, profile):
+     with semaphore:
+         exec(profile)
 
-async def main(delay):
+def main(delay):
     try:
         df=pd.read_excel("account.xlsx",dtype={"query":str,"profile":str,"daily_combo":str},sheet_name='pixelvers')
         df=df[(~df['query'].isna()) & (df['query']!='')]
@@ -299,11 +300,11 @@ async def main(delay):
                 "proxy":row["proxy"],
                 "daily_combo":daily_combo
             }
-            await exec(profile)
+            exec(profile)
         #     profiles.append(profile)
-        # semaphore = asyncio.Semaphore(count_process)  # Limit to 5 concurrent tasks
+        # semaphore = io.Semaphore(count_process)  # Limit to 5 concurrent tasks
         # tasks = [limited_exec(semaphore, profile) for profile in profiles]
-        # await asyncio.gather(*tasks)
+        #  io.gather(*tasks)
         for __second in range(delay*60, 0, -1):
             sys.stdout.write(f"\r{Fore.CYAN}Chờ thời gian nhận tiếp theo trong {Fore.CYAN}{Fore.WHITE}{__second // 60} phút {Fore.WHITE}{__second % 60} giây")
             sys.stdout.flush()
@@ -319,7 +320,7 @@ if __name__ == "__main__":
     delay=int(input("Nhập thời gian nghỉ(phút):"))
     while True:
         try:
-          asyncio.run(main(delay))        
+            main(delay)     
         except Exception as e:
             print_message(traceback.format_exc())
 
